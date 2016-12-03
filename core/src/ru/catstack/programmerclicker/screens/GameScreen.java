a44dd6f5ce19f10c03013e56f5b152630ad8f97e
GORR174 <gorrgorrob@gmail.com>
2016-12-03 21:46:18 +0500

    Timer

--- core/src/ru/catstack/programmerclicker/screens/GameScreen.java
+++ core/src/ru/catstack/programmerclicker/screens/GameScreen.java
@@ -2,12 +2,15 @@ package ru.catstack.programmerclicker.screens;
 
 import ru.catstack.programmerclicker.engine.MyGdxGame;
 import ru.catstack.programmerclicker.objects.interior.*;
+import ru.catstack.programmerclicker.utils.Timer;
 
 public class GameScreen extends Base2DScreen {
 
     public GameScreen(MyGdxGame myGdxGame) {
         super(myGdxGame);
     }
+
+    Timer everySecond = new Timer(1, () -> System.out.println("Every second!"));
 
     @Override
     public void show(){
@@ -18,6 +21,8 @@ public class GameScreen extends Base2DScreen {
     @Override
     public void update(float delta) {
         super.update(delta);
+
+        everySecond.update(delta);
 
         ScreenItems.update();
     }
---
+++ core/src/ru/catstack/programmerclicker/utils/Timer.java
@@ -0,0 +1,58 @@
+package ru.catstack.programmerclicker.utils;
+
+public class Timer {
+
+    private float thisTime = 0;
+    private float finalTime;
+    private Runnable action = new Runnable() {
+        @Override
+        public void run() {
+
+        }
+    };
+    private boolean isPause = false;
+
+    public Timer(float finalTime){
+        this.finalTime = finalTime;
+    }
+
+    public Timer(float finalTime, Runnable action){
+        this.finalTime = finalTime;
+        this.action = action;
+    }
+
+    public void update(float delta){
+
+        if(!isPause)
+            thisTime += delta;
+
+        if(thisTime >= finalTime){
+            thisTime = 0;
+            action.run();
+        }
+    }
+
+    public void pause(){
+        isPause = true;
+    }
+
+    public void unpause(){
+        isPause = false;
+    }
+
+    public void resetTimer(){
+        thisTime = 0;
+    }
+
+    public void setFinalTime(float finalTime) {
+        this.finalTime = finalTime;
+    }
+
+    public void setAction(Runnable action){
+        this.action = action;
+    }
+
+    public Runnable getAction() {
+        return action;
+    }
+}
